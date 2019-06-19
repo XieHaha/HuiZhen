8dbe31f28387a346db667071abc8ac1b9afbc37b
dundun <371523912@qq.com>
2019-06-19 15:00:47 +0800

    处理患者列表侧边bar位置错乱及颜色改变

--- frame/src/main/java/com/yht/frame/widgets/recyclerview/SideBar.java
+++ frame/src/main/java/com/yht/frame/widgets/recyclerview/SideBar.java
@@ -5,25 +5,40 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.yht.frame.R;
 import com.yht.frame.utils.BaseUtils;
+import com.yht.frame.utils.LogUtils;
 
 /**
  * @author MQ
  * @date 2017/5/17
  */
 public class SideBar extends View {
-    private int mHeight;
-    private int mWidth;
-    private Paint mPaint;
-    private int singleHeight;
     private Context mContext;
+    private Paint mPaint;
+    private int mWidth, mHeight;
     private IndexChangeListener listener;
-    private final int TOTAL_MARGIN = 80;
-    private final int TOP_MARGIN = 20;
+    /**
+     * 单个字符高度
+     */
+    private int singleHeight;
+    /**
+     * dp值
+     */
+    private final int TOTAL_MARGIN = 80, TOP_MARGIN = 20;
+    /**
+     * px值
+     */
+    private int marginTop, marginTotal;
+    /**
+     * value
+     */
+    private String indexStr;
 
     public SideBar(Context context) {
         this(context, null);
@@ -40,6 +55,8 @@ public class SideBar extends View {
     }
 
     private void init() {
+        marginTop = BaseUtils.dp2px(mContext, TOP_MARGIN);
+        marginTotal = BaseUtils.dp2px(mContext, TOTAL_MARGIN);
         mPaint = new Paint();
         mPaint.setDither(true);
         mPaint.setAntiAlias(true);
@@ -56,9 +73,11 @@ public class SideBar extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         //导航栏居中显示，上下各有80dp的边距
-        mHeight = (h - BaseUtils.dp2px(mContext, TOTAL_MARGIN));
+        mHeight = (h - marginTotal);
         mWidth = w;
-        singleHeight = BaseUtils.dp2px(mContext, 20);
+        singleHeight = marginTop;
+        mHeight = singleHeight * indexStr.length();
+        LogUtils.i("test", "mWidth:" + mWidth + "  mHeight:" + mHeight + "  singleHeight:" + singleHeight);
     }
 
     @Override
@@ -66,39 +85,47 @@ public class SideBar extends View {
         for (int i = 0; i < indexStr.length(); i++) {
             String textTag = indexStr.substring(i, i + 1);
             float xPos = (mWidth - mPaint.measureText(textTag)) / 2;
-            canvas.drawText(textTag, xPos, singleHeight * (i + 1) + BaseUtils.dp2px(mContext, TOP_MARGIN), mPaint);
+            if (i == curPosition) {
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.color_1491fc));
+            }
+            else {
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.color_373d4d));
+            }
+            canvas.drawText(textTag, xPos, singleHeight * (i + 1) + marginTop, mPaint);
         }
     }
-
-    private String indexStr = "ABCDEFGHIJKLMNOPQRSTUVWXY#";
 
     public void setIndexStr(String indexStr) {
         this.indexStr = indexStr;
     }
 
+    private int curPosition = -1;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                //按下
-                mPaint.setColor(Color.BLACK);
                 invalidate();
             case MotionEvent.ACTION_MOVE:
-                //滑动 event.getY()得到在父View中的Y坐标，通过和总高度的比例再乘以字符个数总长度得到按下的位置  200为margintop距离
-                int position = (int)((event.getY() - getTop() - BaseUtils.dp2px(mContext, 20) + 200) / mHeight *
-                                     indexStr.toCharArray().length);
-                if (position >= 0 && position < indexStr.length()) {
-                    ((IndexBar)getParent()).setDrawData(event.getY(), String.valueOf(indexStr.toCharArray()[position]),
-                                                        position);
-                    if (listener != null) {
-                        listener.indexChanged(indexStr.substring(position, position + 1));
+                //滑动 event.getY()得到在父View中的Y坐标，通过和总高度的比例再乘以字符个数总长度得到按下的位置
+                float y = event.getY() - getTop() - marginTop + 200;
+                if (y > 0) {
+                    curPosition = (int)(y / mHeight * indexStr.toCharArray().length);
+                    if (curPosition >= 0 && curPosition < indexStr.length()) {
+                        ((IndexBar)getParent()).setDrawData(event.getY(),
+                                                            String.valueOf(indexStr.toCharArray()[curPosition]),
+                                                            curPosition);
+                        if (listener != null) {
+                            listener.indexChanged(indexStr.substring(curPosition, curPosition + 1));
+                        }
                     }
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 //抬起
                 ((IndexBar)getParent()).setTagStatus(false);
-                mPaint.setColor(Color.GRAY);
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.color_373d4d));
+                curPosition = -1;
                 invalidate();
                 break;
             default:
