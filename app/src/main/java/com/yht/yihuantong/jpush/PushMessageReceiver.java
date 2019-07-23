d6d88f2e5ee82fb255fb209da9eab1e5d86ada74
dundun <371523912@qq.com>
2019-07-23 18:20:16 +0800

    极光添加接受自定义新消息

--- app/src/main/java/com/yht/yihuantong/jpush/PushMessageReceiver.java
+++ app/src/main/java/com/yht/yihuantong/jpush/PushMessageReceiver.java
@@ -1,134 +1,78 @@
 package com.yht.yihuantong.jpush;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.yht.frame.api.notify.NotifyChangeListenerManager;
-import com.yht.frame.data.CommonData;
-import com.yht.frame.data.type.DocAuthStatus;
-import com.yht.frame.data.type.MessageType;
 import com.yht.frame.utils.HuiZhenLog;
-import com.yht.yihuantong.ZycApplication;
-import com.yht.yihuantong.ui.auth.AuthDoctorActivity;
-import com.yht.yihuantong.ui.main.MainActivity;
-import com.yht.yihuantong.ui.transfer.TransferInitiateDetailActivity;
-import com.yht.yihuantong.ui.transfer.TransferReceiveDetailActivity;
 
-import cn.jpush.android.api.NotificationMessage;
-import cn.jpush.android.service.JPushMessageReceiver;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
+ * 1) 接收自定义消息
+ *
  * @author dundun
  */
-public class PushMessageReceiver extends JPushMessageReceiver implements MessageType {
-    private static final String TAG = "ZYC";
+public class PushMessageReceiver extends BroadcastReceiver {
+    private static final String TAG = "ZYC-PUSH";
 
     @Override
-    public void onNotifyMessageArrived(Context context, NotificationMessage message) {
-        JsonObject jsonObject = new JsonParser().parse(message.notificationExtras).getAsJsonObject();
-        String type = jsonObject.get("msgType").getAsString();
-        String msgId;
-        if (MESSAGE_DOCTOR_AUTH_FAILED.equals(type) || MESSAGE_DOCTOR_AUTH_SUCCESS.equals(type)) {
-            msgId = jsonObject.get("phone").getAsString();
+    public void onReceive(Context context, Intent intent) {
+        try {
+            Bundle bundle = intent.getExtras();
+            HuiZhenLog.i(TAG, "onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+            if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+                JSONObject json = new JSONObject(bundle.getString(JPushInterface.EXTRA_MESSAGE));
+            }
         }
-        else {
-            msgId = jsonObject.get("orderNo").getAsString();
-        }
-        HuiZhenLog.i(TAG, "msgType:" + type + "  msgId:" + msgId);
-        notifyStatusChange(type, msgId);
-    }
-
-    @Override
-    public void onNotifyMessageOpened(Context context, NotificationMessage message) {
-        JsonObject jsonObject = new JsonParser().parse(message.notificationExtras).getAsJsonObject();
-        String type = jsonObject.get("msgType").getAsString();
-        String msgId;
-        if (MESSAGE_DOCTOR_AUTH_FAILED.equals(type) || MESSAGE_DOCTOR_AUTH_SUCCESS.equals(type)) {
-            msgId = jsonObject.get("phone").getAsString();
-        }
-        else {
-            msgId = jsonObject.get("orderNo").getAsString();
-        }
-        HuiZhenLog.i(TAG, "msgType:" + type + "  msgId:" + msgId);
-        jumpPageByType(context, type, msgId);
-    }
-
-    /**
-     * 状态通知
-     *
-     * @param type
-     */
-    private void notifyStatusChange(String type, String msgId) {
-        switch (type) {
-            case MESSAGE_DOCTOR_AUTH_SUCCESS:
-                NotifyChangeListenerManager.getInstance().notifyDoctorAuthStatus(DocAuthStatus.AUTH_SUCCESS);
-                break;
-            case MESSAGE_DOCTOR_AUTH_FAILED:
-                NotifyChangeListenerManager.getInstance().notifyDoctorAuthStatus(DocAuthStatus.AUTH_FAILD);
-                break;
-            default:
-                break;
+        catch (Exception e) {
+            HuiZhenLog.e(TAG, "Exception error", e);
         }
     }
 
     /**
-     * 通知栏业务处理
+     * 打印所有的 intent extra 数据
      *
-     * @param type
+     * @param bundle a
+     * @return a
      */
-    private void jumpPageByType(Context context, String type, String msgId) {
-        if (ZycApplication.getInstance().getLoginBean() == null) {
-            return;
+    private static String printBundle(Bundle bundle) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_NOTIFICATION_ID)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getInt(key));
+            }
+            else if (key.equals(JPushInterface.EXTRA_CONNECTION_CHANGE)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getBoolean(key));
+            }
+            else if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                if (TextUtils.isEmpty(bundle.getString(JPushInterface.EXTRA_EXTRA))) {
+                    HuiZhenLog.i(TAG, "This message has no Extra data");
+                    continue;
+                }
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(JPushInterface.EXTRA_EXTRA));
+                    Iterator<String> it = json.keys();
+                    while (it.hasNext()) {
+                        String myKey = it.next();
+                        sb.append("\nkey:" + key + ", value: [" + myKey + " - " + json.optString(myKey) + "]");
+                    }
+                }
+                catch (JSONException e) {
+                    HuiZhenLog.e(TAG, "Get message extra JSON error!");
+                }
+            }
+            else {
+                sb.append("\nkey:" + key + ", value:" + bundle.getString(key));
+            }
         }
-        Intent mainIntent, baseIntent;
-        Intent[] intents;
-        switch (type) {
-            case MESSAGE_DOCTOR_AUTH_SUCCESS:
-            case MESSAGE_DOCTOR_AUTH_FAILED:
-                mainIntent = new Intent(context, AuthDoctorActivity.class);
-                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(mainIntent);
-                break;
-            case MESSAGE_SERVICE_REPORT:
-                mainIntent = new Intent(context, MainActivity.class);
-                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
-                baseIntent = new Intent(context, AuthDoctorActivity.class);
-                intents = new Intent[] { mainIntent, baseIntent };
-                context.startActivities(intents);
-                break;
-            case MESSAGE_TRANSFER_UPDATE:
-                break;
-            case MESSAGE_TRANSFER_APPLY:
-            case MESSAGE_TRANSFER_REJECT:
-            case MESSAGE_TRANSFER_RECEIVED:
-            case MESSAGE_TRANSFER_OTHER:
-            case MESSAGE_TRANSFER_SYSTEM_CANCEL_T:
-                mainIntent = new Intent(context, MainActivity.class);
-                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
-                baseIntent = new Intent(context, TransferInitiateDetailActivity.class);
-                baseIntent.putExtra(CommonData.KEY_ORDER_ID, msgId);
-                intents = new Intent[] { mainIntent, baseIntent };
-                context.startActivities(intents);
-                break;
-            case MESSAGE_TRANSFER_CANCEL:
-            case MESSAGE_TRANSFER_SYSTEM_CANCEL_R:
-                mainIntent = new Intent(context, MainActivity.class);
-                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
-                baseIntent = new Intent(context, TransferReceiveDetailActivity.class);
-                baseIntent.putExtra(CommonData.KEY_ORDER_ID, msgId);
-                intents = new Intent[] { mainIntent, baseIntent };
-                context.startActivities(intents);
-                break;
-            case MESSAGE_CURRENCY_ARRIVED:
-            case MESSAGE_CURRENCY_DEDUCTION:
-            case MESSAGE_ACCOUNT_CREATE:
-            default:
-                break;
-        }
+        return sb.toString();
     }
 }
---
+++ app/src/main/java/com/yht/yihuantong/jpush/PushNotifyReceiver.java
@@ -0,0 +1,135 @@
+package com.yht.yihuantong.jpush;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.yht.frame.api.notify.NotifyChangeListenerManager;
+import com.yht.frame.data.CommonData;
+import com.yht.frame.data.type.DocAuthStatus;
+import com.yht.frame.data.type.MessageType;
+import com.yht.frame.utils.HuiZhenLog;
+import com.yht.yihuantong.ZycApplication;
+import com.yht.yihuantong.ui.auth.AuthDoctorActivity;
+import com.yht.yihuantong.ui.main.MainActivity;
+import com.yht.yihuantong.ui.transfer.TransferInitiateDetailActivity;
+import com.yht.yihuantong.ui.transfer.TransferReceiveDetailActivity;
+
+import cn.jpush.android.api.NotificationMessage;
+import cn.jpush.android.service.JPushMessageReceiver;
+
+/**
+ * @author dundun
+ * 通知消息
+ */
+public class PushNotifyReceiver extends JPushMessageReceiver implements MessageType {
+    private static final String TAG = "ZYC";
+
+    @Override
+    public void onNotifyMessageArrived(Context context, NotificationMessage message) {
+        JsonObject jsonObject = new JsonParser().parse(message.notificationExtras).getAsJsonObject();
+        String type = jsonObject.get("msgType").getAsString();
+        String msgId;
+        if (MESSAGE_DOCTOR_AUTH_FAILED.equals(type) || MESSAGE_DOCTOR_AUTH_SUCCESS.equals(type)) {
+            msgId = jsonObject.get("phone").getAsString();
+        }
+        else {
+            msgId = jsonObject.get("orderNo").getAsString();
+        }
+        HuiZhenLog.i(TAG, "msgType:" + type + "  msgId:" + msgId);
+        notifyStatusChange(type, msgId);
+    }
+
+    @Override
+    public void onNotifyMessageOpened(Context context, NotificationMessage message) {
+        JsonObject jsonObject = new JsonParser().parse(message.notificationExtras).getAsJsonObject();
+        String type = jsonObject.get("msgType").getAsString();
+        String msgId;
+        if (MESSAGE_DOCTOR_AUTH_FAILED.equals(type) || MESSAGE_DOCTOR_AUTH_SUCCESS.equals(type)) {
+            msgId = jsonObject.get("phone").getAsString();
+        }
+        else {
+            msgId = jsonObject.get("orderNo").getAsString();
+        }
+        HuiZhenLog.i(TAG, "msgType:" + type + "  msgId:" + msgId);
+        jumpPageByType(context, type, msgId);
+    }
+
+    /**
+     * 状态通知
+     *
+     * @param type
+     */
+    private void notifyStatusChange(String type, String msgId) {
+        switch (type) {
+            case MESSAGE_DOCTOR_AUTH_SUCCESS:
+                NotifyChangeListenerManager.getInstance().notifyDoctorAuthStatus(DocAuthStatus.AUTH_SUCCESS);
+                break;
+            case MESSAGE_DOCTOR_AUTH_FAILED:
+                NotifyChangeListenerManager.getInstance().notifyDoctorAuthStatus(DocAuthStatus.AUTH_FAILD);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 通知栏业务处理
+     *
+     * @param type
+     */
+    private void jumpPageByType(Context context, String type, String msgId) {
+        if (ZycApplication.getInstance().getLoginBean() == null) {
+            return;
+        }
+        Intent mainIntent, baseIntent;
+        Intent[] intents;
+        switch (type) {
+            case MESSAGE_DOCTOR_AUTH_SUCCESS:
+            case MESSAGE_DOCTOR_AUTH_FAILED:
+                mainIntent = new Intent(context, AuthDoctorActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(mainIntent);
+                break;
+            case MESSAGE_SERVICE_REPORT:
+                mainIntent = new Intent(context, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
+                baseIntent = new Intent(context, AuthDoctorActivity.class);
+                intents = new Intent[] { mainIntent, baseIntent };
+                context.startActivities(intents);
+                break;
+            case MESSAGE_TRANSFER_UPDATE:
+                break;
+            case MESSAGE_TRANSFER_APPLY:
+            case MESSAGE_TRANSFER_REJECT:
+            case MESSAGE_TRANSFER_RECEIVED:
+            case MESSAGE_TRANSFER_OTHER:
+            case MESSAGE_TRANSFER_SYSTEM_CANCEL_T:
+                mainIntent = new Intent(context, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
+                baseIntent = new Intent(context, TransferInitiateDetailActivity.class);
+                baseIntent.putExtra(CommonData.KEY_ORDER_ID, msgId);
+                intents = new Intent[] { mainIntent, baseIntent };
+                context.startActivities(intents);
+                break;
+            case MESSAGE_TRANSFER_CANCEL:
+            case MESSAGE_TRANSFER_SYSTEM_CANCEL_R:
+                mainIntent = new Intent(context, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mainIntent.putExtra(CommonData.KEY_PUBLIC, 0);
+                baseIntent = new Intent(context, TransferReceiveDetailActivity.class);
+                baseIntent.putExtra(CommonData.KEY_ORDER_ID, msgId);
+                intents = new Intent[] { mainIntent, baseIntent };
+                context.startActivities(intents);
+                break;
+            case MESSAGE_CURRENCY_ARRIVED:
+            case MESSAGE_CURRENCY_DEDUCTION:
+            case MESSAGE_ACCOUNT_CREATE:
+            default:
+                break;
+        }
+    }
+}
