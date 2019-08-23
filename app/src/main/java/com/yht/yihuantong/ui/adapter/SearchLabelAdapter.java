6ed9ba6d612e0d1fa4746b8352a3e98bc7fec8fe
dundun <371523912@qq.com>
2019-08-23 17:11:21 +0800

    标签搜索完成, 标签是否更改校验完成

---
+++ app/src/main/java/com/yht/yihuantong/ui/adapter/SearchLabelAdapter.java
@@ -0,0 +1,103 @@
+package com.yht.yihuantong.ui.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.yht.yihuantong.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author 顿顿
+ * @date 19/8/12 17:56
+ * @des 标签  搜索候选
+ */
+public class SearchLabelAdapter extends BaseAdapter {
+    private Context context;
+    private String searchKey;
+    private List<String> list = new ArrayList<>();
+
+    public SearchLabelAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setList(List<String> list) {
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_search_label, parent, false);
+            holder.tvName = convertView.findViewById(R.id.tv_label_name);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+        init(holder, list.get(position));
+        return convertView;
+    }
+
+    private void init(ViewHolder holder, String value) {
+        holder.tvName.setText(setKeyWordColor(value, searchKey == null ? "" : searchKey));
+    }
+
+    /**
+     * 设置搜索关键字高亮
+     *
+     * @param content 原文本内容
+     */
+    private SpannableString setKeyWordColor(String content, String searchKey) {
+        SpannableString s = new SpannableString(content);
+        Pattern p = Pattern.compile(searchKey);
+        Matcher m = p.matcher(s);
+        while (m.find()) {
+            int start = m.start();
+            int end = m.end();
+            s.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.color_1491fc)), start, end,
+                      Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        return s;
+    }
+
+    private class ViewHolder {
+        private TextView tvName;
+    }
+}
--- app/src/main/java/com/yht/yihuantong/ui/patient/EditLabelActivity.java
+++ app/src/main/java/com/yht/yihuantong/ui/patient/EditLabelActivity.java
@@ -4,13 +4,16 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.AppCompatEditText;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.yht.frame.api.notify.NotifyChangeListenerManager;
@@ -22,8 +25,12 @@ import com.yht.frame.data.bean.LabelSetBean;
 import com.yht.frame.http.retrofit.RequestUtils;
 import com.yht.frame.ui.BaseActivity;
 import com.yht.frame.utils.BaseUtils;
+import com.yht.frame.utils.HuiZhenLog;
 import com.yht.frame.utils.ToastUtil;
+import com.yht.frame.widgets.dialog.HintDialog;
+import com.yht.frame.widgets.edittext.AbstractTextWatcher;
 import com.yht.yihuantong.R;
+import com.yht.yihuantong.ui.adapter.SearchLabelAdapter;
 import com.zhy.view.flowlayout.FlowLayout;
 import com.zhy.view.flowlayout.TagAdapter;
 import com.zhy.view.flowlayout.TagFlowLayout;
@@ -42,7 +49,8 @@ import butterknife.OnClick;
  * @des 标签编辑
  */
 public class EditLabelActivity extends BaseActivity
-        implements TextView.OnEditorActionListener, View.OnKeyListener, TagFlowLayout.OnTagClickListener {
+        implements TextView.OnEditorActionListener, View.OnKeyListener, TagFlowLayout.OnTagClickListener,
+                   AdapterView.OnItemClickListener {
     @BindView(R.id.selected_flow)
     FlowLayout selectedFlow;
     @BindView(R.id.all_flow)
@@ -51,6 +59,14 @@ public class EditLabelActivity extends BaseActivity
     LinearLayout layoutAll;
     @BindView(R.id.public_title_bar_more)
     TextView publicTitleBarMore;
+    @BindView(R.id.list_view)
+    ListView listView;
+    @BindView(R.id.public_title_bar_back)
+    ImageView publicTitleBarBack;
+    /**
+     * 搜索候选
+     */
+    private SearchLabelAdapter searchLabelAdapter;
     /**
      * 当前患者
      */
@@ -62,19 +78,19 @@ public class EditLabelActivity extends BaseActivity
     /**
      * 标签输入框
      */
-    private AppCompatEditText inputEditText;
+    private EditText inputEditText;
     /**
      * 当前处于高亮状态的TextView
      */
     private TextView curHighLightTextView;
     /**
-     * 当前高亮状态view的index
-     */
-    private int curHighLightPosition;
-    /**
      * 高亮状态
      */
     private boolean curHighLight = false;
+    /**
+     * 已有标签初始值（判断本次编辑是否修改）
+     */
+    private String initString;
     /**
      * 上面的标签列表
      */
@@ -83,6 +99,10 @@ public class EditLabelActivity extends BaseActivity
      * 所有标签列表
      */
     private List<String> allLabelList = new ArrayList<>();
+    /**
+     * 搜索结果标签
+     */
+    private List<String> searchLabels = new ArrayList<>();
     /**
      * 存放已有标签
      */
@@ -134,18 +154,29 @@ public class EditLabelActivity extends BaseActivity
             }
         };
         allFlow.setAdapter(tagAdapter);
+        searchLabelAdapter = new SearchLabelAdapter(this);
+        listView.setOnItemClickListener(this);
+        listView.setAdapter(searchLabelAdapter);
         getAllLabel();
-        initLabelData();
     }
 
     @Override
     public void initListener() {
         super.initListener();
+        publicTitleBarBack.setOnClickListener(this);
         //重新显示光标
         inputEditText.setOnClickListener(v -> {
             inputEditText.setCursorVisible(true);
             //输入框重新获取焦点后，取消标签高亮状态
             setLabelHighLight(false);
+        });
+        inputEditText.addTextChangedListener(new AbstractTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                super.onTextChanged(s, start, before, count);
+                HuiZhenLog.i(TAG, "value:" + s.toString());
+                searchLabel(s.toString());
+            }
         });
         //给候选标签添加监听
         allFlow.setOnTagClickListener(this);
@@ -159,7 +190,7 @@ public class EditLabelActivity extends BaseActivity
     }
 
     /**
-     * 获取所有标签
+     * 保存所有标签
      */
     private void savePatientLabel() {
         RequestUtils.savePatientLabel(this, loginBean.getToken(), patientCode, selectedLabelList, this);
@@ -177,9 +208,13 @@ public class EditLabelActivity extends BaseActivity
             }
             ArrayList<LabelBean> list = labelSetBean.getPatientTag();
             if (list != null) {
-                for (int i = 0; i < list.size(); i++) {
-                    //初始化已有标签
-                    addLabel(list.get(i).getTagName());
+                StringBuilder builder = new StringBuilder();
+                for (LabelBean bean : list) {
+                    builder.append(bean.getTagName());
+                    //保存已有标签初始值
+                    initString = builder.toString();
+                    //添加标签
+                    addLabel(bean.getTagName());
                 }
             }
         }
@@ -210,6 +245,40 @@ public class EditLabelActivity extends BaseActivity
         tagAdapter.notifyDataChanged();
     }
 
+    /**
+     * 患者搜索
+     */
+    private void searchLabel(String tag) {
+        if (!TextUtils.isEmpty(tag)) {
+            searchLabels.clear();
+            for (String string : allLabelList) {
+                if (string.contains(tag)) {
+                    searchLabels.add(string);
+                }
+            }
+            initSearchList(searchLabels, tag);
+        }
+        else {
+            initSearchList(null, "");
+        }
+    }
+
+    /**
+     * 搜索结果处理
+     */
+    private void initSearchList(List<String> list, String tag) {
+        if (list != null && list.size() > 0) {
+            listView.setVisibility(View.VISIBLE);
+            layoutAll.setVisibility(View.GONE);
+            searchLabelAdapter.setSearchKey(tag);
+            searchLabelAdapter.setList(list);
+        }
+        else {
+            layoutAll.setVisibility(View.VISIBLE);
+            listView.setVisibility(View.GONE);
+        }
+    }
+
     @OnClick(R.id.public_title_bar_more)
     public void onViewClicked() {
         if (publicTitleBarMore.isSelected()) {
@@ -223,8 +292,26 @@ public class EditLabelActivity extends BaseActivity
     @Override
     public void onClick(View v) {
         super.onClick(v);
-        TextView textView = (TextView)v;
-        removeSelectedLabel(textViewLabels.indexOf(textView), textView);
+        switch (v.getId()) {
+            case R.id.tv_label:
+                TextView textView = (TextView)v;
+                removeSelectedLabel(textViewLabels.indexOf(textView), textView);
+                break;
+            case R.id.public_title_bar_back:
+                onFinish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 搜索候选列表
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        addLabel(searchLabels.get(position));
+        hideSoftInputFromWindow();
     }
 
     /**
@@ -273,12 +360,11 @@ public class EditLabelActivity extends BaseActivity
         String s = inputEditText.getText().toString();
         if (textViewLabels.size() == 0 || !TextUtils.isEmpty(s)) { return false; }
         if (keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_DOWN) {
-            curHighLightPosition = textViewLabels.size() - 1;
-            curHighLightTextView = textViewLabels.get(curHighLightPosition);
+            curHighLightTextView = textViewLabels.get(textViewLabels.size() - 1);
             if (curHighLight) {
                 //显示光标
                 inputEditText.setCursorVisible(true);
-                removeSelectedLabel(curHighLightPosition, curHighLightTextView);
+                removeSelectedLabel(textViewLabels.size() - 1, curHighLightTextView);
             }
             else {
                 //隐藏输入框光标
@@ -312,9 +398,8 @@ public class EditLabelActivity extends BaseActivity
         }
         //重新将新的标签添加在最后（顺序重排）
         selectedLabelList.add(content);
-        //
-        publicTitleBarMore.setSelected(true);
         updateAllLabelSelectStatus();
+        initNextButton();
     }
 
     /**
@@ -367,6 +452,7 @@ public class EditLabelActivity extends BaseActivity
         selectedFlow.removeView(target);
         textViewLabels.remove(position);
         updateAllLabelSelectStatus();
+        initNextButton();
     }
 
     /**
@@ -386,12 +472,48 @@ public class EditLabelActivity extends BaseActivity
     /**
      * 创建输入标签
      */
-    private AppCompatEditText createInputLabel() {
-        inputEditText = (AppCompatEditText)getLayoutInflater().inflate(R.layout.item_edit_label, null, false);
+    private EditText createInputLabel() {
+        inputEditText = (EditText)getLayoutInflater().inflate(R.layout.item_edit_label, null, false);
         inputEditText.setOnEditorActionListener(this);
         inputEditText.setOnKeyListener(this);
         inputEditText.setLayoutParams(params);
         return inputEditText;
+    }
+
+    private void onFinish() {
+        if (publicTitleBarMore.isSelected()) {
+            new HintDialog(this).setContentString(R.string.txt_save_edit)
+                                .setCancleBtnTxt(R.string.txt_not_save)
+                                .setOnCancelClickListener(this::finish)
+                                .setEnterBtnTxt(R.string.txt_save)
+                                .setEnterSelect(true)
+                                .setOnEnterClickListener(this::savePatientLabel)
+                                .show();
+            return;
+        }
+        hideSoftInputFromWindow();
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        onFinish();
+    }
+
+    /**
+     * 是否保存
+     */
+    private void initNextButton() {
+        StringBuilder builder = new StringBuilder();
+        for (String string : selectedLabelList) {
+            builder.append(string);
+        }
+        if (TextUtils.equals(builder.toString(), initString)) {
+            publicTitleBarMore.setSelected(false);
+        }
+        else {
+            publicTitleBarMore.setSelected(true);
+        }
     }
 
     @Override
